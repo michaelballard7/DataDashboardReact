19761b1b4990bf5a66f30b3914d551f73909d497
Michael Anthony Ballard <MichaelBallard@Michaels-MBP-4.fios-router.home>
2018-10-15 22:46:34 -0400

    Updated asset selection

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import styled,{css} from 'styled-components'
 import AppBar from './appbar.js'
 import CoinList from './CoinList.js'
+import _ from 'lodash'
 const cc = require('cryptocompare')
 
 const AppLayout = styled.div`
@@ -11,6 +12,8 @@ const AppLayout = styled.div`
 
 const Content = styled.div` 
 `
+
+const MAX_FAVORITES = 10
 
 const checkFirstVisit = () => {
   let cryptoDash = localStorage.getItem('assetDash')
@@ -28,7 +31,8 @@ class App extends Component {
   // set state in this object which is refereence in the component render
   state = {
     page: 'settings',
-    ...checkFirstVisit()
+    favorites: ['ETH', 'BTC', 'XMR', 'DOGE', 'EOS'],
+    ...checkFirstVisit()  // study the uses of the spread
   }
 
 
@@ -70,6 +74,7 @@ class App extends Component {
           Choose Favorite Coins: 
         </div>
         <div>
+          {CoinList.call(this, true)}
           {CoinList.call(this)}
         </div>
       </div>
@@ -84,6 +89,26 @@ class App extends Component {
     } 
   }
 
+
+  addCoinToFavorites = (key) => {
+    console.log('Adding', key)
+    let favorites = [...this.state.favorites];
+    if (favorites.length < MAX_FAVORITES){
+      favorites.push(key)
+      this.setState({favorites})
+    }
+  }
+
+  removeCoinFromFavorites = (key) => {
+    console.log('Removing', key)
+    let favorites = [...this.state.favorites];
+    this.setState({favorites:_.pull(favorites, key)}) //low dash call .pull()
+  }
+
+  isInFavorites = (key)=>{
+    return _.includes(this.state.favorites, key)
+
+  }
 
   render() {
     // here are the child components
--- src/CoinList.js
+++ src/CoinList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled,{css} from 'styled-components'
-import {subtleBoxShadow,greenBoxShadow,lightBlueBackground} from './Style'
+import {subtleBoxShadow,greenBoxShadow, redBoxShadow, lightBlueBackground} from './Style'
 
 
 // create the css grid layout
@@ -19,6 +19,20 @@ const CoinTile = styled.div`
         cursor: pointer;
         ${greenBoxShadow}
     }
+
+    ${props => props.favorite && css`
+        &:hover{
+            cursor: pointer;
+            ${redBoxShadow}
+        }
+
+    `}
+
+    ${props => props.chosen && !props.favorite && css`
+    
+        pointer-events: none;
+        opacity: 0.4;
+    `}
 `
 
 const CoinHeaderGrid = styled.div`
@@ -31,16 +45,27 @@ const CoinSymbol = styled.div`
     justify-self: right;
 `
 
-export default function(){
-    console.log('Sammple', this.state.coinList['BTC'])
+
+const DeleteIcon = styled.div`
+    justify-self: right;
+    display: none;
+    ${CoinTile}:hover & {
+        color: red;
+        display: block;
+    }
+`
+export default function(favorites=false){
+    let coinKeys = favorites ? this.state.favorites : Object.keys(this.state.coinList).slice(0,100);
     return <CoinGrid>
-        {Object.keys(this.state.coinList).slice(0,1000).map(coin =>
-            <CoinTile>
+        {coinKeys.map(coinKey =>
+            <CoinTile chosen={this.isInFavorites(coinKey)} favorite={favorites} onClick={
+                favorites ? ()=>{this.removeCoinFromFavorites(coinKey)} : ()=>{this.addCoinToFavorites(coinKey)}}>
                 <CoinHeaderGrid>
-                    <div> {this.state.coinList[coin].CoinName}</div>
-                    <CoinSymbol> {this.state.coinList[coin].Symbol}</CoinSymbol>
+                    <div> {this.state.coinList[coinKey].CoinName}</div>
+                   { favorites ? <DeleteIcon> X </DeleteIcon> : <CoinSymbol> {this.state.coinList[coinKey].Symbol}</CoinSymbol>} 
                </CoinHeaderGrid>
-                {<img style={{height: '50px'}} src={`http://cryptocompare.com/${this.state.coinList[coin].ImageUrl}`}/>}
+                {<img style={{height: '50px'}} src={`http://cryptocompare.com/${this.state.coinList[coinKey].ImageUrl}`}/>}
+
             </CoinTile>
             
             )}
