6b0bfb3f381175c89e08081019cbd087b8eb746f
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro-2.local>
2019-02-18 18:28:09 -0500

    Updates

---
+++ precache-manifest.790e2b179e73f92bcf58be07842145e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81ecda057d06f6fc1f23",
+    "url": "/DataDashboardReact/static/css/main.518fb0aa.chunk.css"
+  },
+  {
+    "revision": "81ecda057d06f6fc1f23",
+    "url": "/DataDashboardReact/static/js/main.81ecda05.chunk.js"
+  },
+  {
+    "revision": "50a6d5a28fe9be5d291a",
+    "url": "/DataDashboardReact/static/js/1.50a6d5a2.chunk.js"
+  },
+  {
+    "revision": "89b519c62a7bf9410a21",
+    "url": "/DataDashboardReact/static/js/runtime~main.89b519c6.js"
+  },
+  {
+    "revision": "1db418774908140e13eda66c5d6c0875",
+    "url": "/DataDashboardReact/index.html"
+  }
+];
